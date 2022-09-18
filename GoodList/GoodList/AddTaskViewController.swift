44e0859109de4b052b2d91151966fc0e33f50ec1
applebuddy <eagermin92@gmail.com>
2022-09-19 02:00:21 +0900

    Study Lecture 42. Passing Task to Task List Screen

--- GoodList/GoodList/AddTaskViewController.swift
+++ GoodList/GoodList/AddTaskViewController.swift
@@ -11,6 +11,12 @@ import RxSwift
 class AddTaskViewController: UIViewController {
   @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
   @IBOutlet weak var taskTitleTextField: UITextField!
+  
+  // Task Model 데이터를 저장, 전달하기 위해서 Subject를 사용합니다.
+  private let taskSubject = PublishSubject<Task>()
+  var taskSubjectObservable: Observable<Task> {
+    return taskSubject.asObservable()
+  }
 
   @IBAction func save() {
     guard let priority = Priority(rawValue: self.prioritySegmentedControl.selectedSegmentIndex),
@@ -20,6 +26,8 @@ class AddTaskViewController: UIViewController {
     
     // delegate 패턴을 통해 값을 전달할 수도 있지만, RxSwift를 활용해서 값을 전달해보자.
     let task = Task(title: title, priority: priority)
-    
+    // taskSubject를 구독한 곳에서는 특정 task 모델 데이터를 받게 된다.
+    taskSubject.onNext(task)
+    self.dismiss(animated: true)
   }
 }
--- GoodList/GoodList/TaskListViewController.swift
+++ GoodList/GoodList/TaskListViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import RxSwift
 
 class TaskListViewController: UIViewController {
   @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
   @IBOutlet weak var tableView: UITableView!
+  private let disposeBag = DisposeBag()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // RxSwift 활용, AddTaskViewController의 taskObservable을 구독해서 특정 Task를 저장할때 그 값을 전달받을 수 있도록 할 수 있다.
+    guard let navigationController = segue.destination as? UINavigationController,
+          let addViewController = navigationController.viewControllers.first as? AddTaskViewController else {
+      fatalError("ViewController not found.....")
+    }
+    addViewController.taskSubjectObservable.subscribe(onNext: { task in
+      // taskSubject 이벤트를 구독하여 taskSubject 이벤트가 발생할때 Task 모델 이벤트를 받아 처리할 수 있다.
+      print(task)
+    }).disposed(by: disposeBag)
   }
 }
 

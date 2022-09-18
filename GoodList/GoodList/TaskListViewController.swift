72358ece794137b2d1dadfd24b5aec5bc7f86bc9
applebuddy <eagermin92@gmail.com>
2022-09-19 01:15:54 +0900

    Study Lecture 37 ~ 38. Designing the Task List Screen

---
+++ GoodList/GoodList/TaskListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TaskListViewController.swift
+//  GoodList
+//
+//  Created by MinKyeongTae on 2022/09/19.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+  @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+  @IBOutlet weak var tableView: UITableView!
+}
+
+extension TaskListViewController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: TaskTableViewCell.self), for: indexPath) as? TaskTableViewCell else {
+//      return UITableViewCell()
+//    }
+//
+//    return cell
+    return UITableViewCell()
+  }
+}
---
+++ GoodList/GoodList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by MinKyeongTae on 2022/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+

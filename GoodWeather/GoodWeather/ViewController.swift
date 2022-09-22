c82d118db667282bc3c7600095e44cbc08112558
applebuddy <eagermin92@gmail.com>
2022-09-23 04:43:05 +0900

    Study Lecture 75. Fetching and Displaying Weather on Search

--- GoodWeather/GoodWeather/ViewController.swift
+++ GoodWeather/GoodWeather/ViewController.swift
@@ -19,7 +19,27 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    // 텍스트필드 편집을 마쳤을때에 API 호출을 해서 UI 업데이트 한다.
+    self.cityNameTextField.rx.controlEvent(.editingDidEndOnExit) // CcntrolEvent<()>
+      .asObservable() // -> Observable<()>
+      .map { self.cityNameTextField.text }
+      .subscribe(onNext: { [weak self] city in
+        if let city = city {
+          if city.isEmpty {
+            DispatchQueue.main.async {
+              self?.displayWeather(nil)
+            }
+          } else {
+            self?.fetchWeather(by: city)
+          }
+        }
+      }).disposed(by: disposeBag)
+    
     // cityNameTextField.rx.value를 구독하면 텍스트필드가 입력될때마다 입력된 값을 감지합니다.
+    // 텍스트가 입력될때마다 API를 호출하는 것은 비효율적이다. 버튼이나 텍스트필드를 터치했을때 API 결과를 받아오는게 어떨까
+    // 텍스트 문자 입력 시마다 API호출 되는게 아닌, 편집 종료시에만 1회 호출 후 UI 업데이트
+    /*
     self.cityNameTextField.rx.value
       .subscribe(onNext: { [weak self] city in
         if let city = city {
@@ -30,6 +50,7 @@ class ViewController: UIViewController {
           }
         }
       }).disposed(by: disposeBag)
+     */
   }
   
   private func fetchWeather(by city: String) {
